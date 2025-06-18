95b604b5fbe0088d221888123957e5e8ae0734c6
Pankeet <pankeet04@gmail.com>
2025-06-18 10:31:05 +0530

    Animation

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,16 +1,28 @@
-"use client"
-import { Link , useLocation } from 'react-router-dom';
+"use client";
+import { Link, useLocation } from 'react-router-dom';
 import { motion } from "motion/react";
+import gsap from 'gsap';
+import { useLayoutEffect } from 'react';
 
-export default function Header({theme , setheme}){
 
+export default function Header({ theme, setheme }) {
   const location = useLocation();
-  console.log(location.pathname)
-    
-  function ldmode(){
+
+  useLayoutEffect(()=>{
+    const tl = gsap.timeline();
+    tl.from("div" , {
+      y:-30,
+      duration:0.3,
+      opacity:0,
+      delay:0.2,
+      stagger:0.2
+    })
+  },[])
+
+  function ldmode() {
     setheme(current => !current);
   }
-    
+
   const container = {
     width: 50,
     height: 25,
@@ -19,44 +31,43 @@ export default function Header({theme , setheme}){
     cursor: "pointer",
     display: "flex",
     padding: 2.5,
-  }
-  
+  };
+
   const handle = {
     width: 20,
     height: 20,
     backgroundColor: "black",
     borderRadius: "50%",
-  }
+  };
 
-      return (
-        <div className="font-serif flex text-xl my-3 mx-3 pb-2">
-            <div><Link to="/" className='headerCSS'><span className={`${location.pathname == '/' || '' ? 'activeHeader' : '' } hover:text-[#b74b4b] hover:border-b-2 hover:border-b-[#b74b4b]`}>Home</span></Link></div>
-            <div><Link to="/about" className="headerCSS"><span className={`${location.pathname == '/about' ? 'activeHeader' : '' } hover:text-[#b74b4b] hover:border-b-2 hover:border-b-[#b74b4b]`}>About Me</span></Link></div>
-            <div><Link to="/projects" className="headerCSS"><span className={`${location.pathname === '/projects' ? 'activeHeader' : ''} hover:text-[#b74b4b] hover:border-b-2 hover:border-b-[#b74b4b] `}>Projects</span></Link></div>
-            <div><a href='https://contact-me-flax-nu.vercel.app/' className='headerCSS hover:text-[#b74b4b] hover:border-b-2 hover:border-b-[#b74b4b]'>Contact</a></div>
-  
-          <div className = 'ml-auto mr-10'>
-          <button className="toggle-container"
+  return (
+    <div className="font-serif flex text-xl my-3 mx-3 pb-2">
+      <div><Link to="/" className='headerCSS'><span className={`${location.pathname === '/' ? 'activeHeader' : ''} hover:text-[#b74b4b]  hover:border-b-2 hover:border-b-[#b74b4b]`}>Home</span></Link></div>
+      <div><Link to="/about" className="headerCSS"><span className={`${location.pathname === '/about' ? 'activeHeader' : ''} hover:text-[#b74b4b] hover:border-b-2 hover:border-b-[#b74b4b]`}>About Me</span></Link></div>
+      <div><Link to="/projects" className="headerCSS"><span className={`${location.pathname === '/projects' ? 'activeHeader' : ''} hover:text-[#b74b4b] hover:border-b-2 hover:border-b-[#b74b4b] `}>Projects</span></Link></div>
+      <div><span><a href='https://contact-me-flax-nu.vercel.app/' className='headerCSS hover:text-[#b74b4b] hover:border-b-2 hover:border-b-[#b74b4b]'>Contact</a></span></div>
+
+      <div className='ml-auto mr-10'>
+        <button
+          className="toggle-container"
           style={{
             ...container,
             justifyContent: "flex-" + (theme ? "start" : "end"),
+          }}
+          onClick={ldmode}
+        >
+          <motion.div
+            className="toggle-handle"
+            style={handle}
+            layout
+            transition={{
+              type: "spring",
+              visualDuration: 0.3,
+              bounce: 0.2,
             }}
-            onClick={ldmode}>
-
-            <motion.div
-                className="toggle-handle"
-                style={handle}
-                layout
-                transition={{
-                  type: "spring",
-                    visualDuration: 0.3,
-                    bounce: 0.2,
-                  }}
-            />
+          />
         </button>
-          </div>
-        </div>
-      )
-    }
-  
-  
+      </div>
+    </div>
+  );
+}
